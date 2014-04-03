d1db42501749d73df1d3cd38eedd1bf8711e1085
faustineinsun <faustine.yu@gmail.com>
2014-04-02 22:40:26 -0700

    Spring MVC + Tomcat + Maven + Eclipse

--- WiseCrowdRec/src/main/java/com/feiyu/controller/MainController.java
+++ WiseCrowdRec/src/main/java/com/feiyu/controller/MainController.java
@@ -4,15 +4,38 @@ package com.feiyu.controller;
   * From http://www.mkyong.com/maven/how-to-create-a-web-application-project-with-maven/  
   */
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class MainController {
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String tweetsAnalyzer(HttpServletRequest req, HttpServletResponse resp,Locale locale, Model model) throws Exception {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "index"; // tweetsanalyzer
+	}
 
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
---
+++ WiseCrowdRec/src/main/webapp/WEB-INF/jspviews/index.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                <title>Hello</title>
+        </head>
+        <body>
+                <h1>Time: ${serverTime}</h1>
+                <br>
+                <h1>Message: ${message}</h1>
+        </body>
+</html>
